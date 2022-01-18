7fc691b88eac5aec59f6bb99c6784d38fbc0641d
paulvonoberstein <78957609+paulvonoberstein@users.noreply.github.com>
2022-01-18 22:22:16 +1300

    added start game button and startGame function

--- index.js
+++ index.js
@@ -6,21 +6,23 @@ let firstCard = 2;
 let secondCard = 19;
 let hasBlackjack = false;
 let isAlive = true;
-let message;
-
+let message = ""
 let sum = firstCard + secondCard
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
+function startGame() {
 
-} else if (sum === 21) {
-    message = "Rejoice boy, for your wish has come true.";
-    hasBlackjack = true;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
 
-} else {
-    message = "You are out of the game.";
-    isAlive = false;
+    } else if (sum === 21) {
+        message = "Rejoice boy, for your wish has come true.";
+        hasBlackjack = true;
 
+    } else {
+        message = "You are out of the game.";
+        isAlive = false;
+
+    }
 }
 
 console.log(message);
