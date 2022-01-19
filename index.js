59e2880fbc259b0ecc57739914323257e9825419
paulvonoberstein <78957609+paulvonoberstein@users.noreply.github.com>
2022-01-19 21:05:17 +1300

    I forgot

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
 // Set their values to a random number between 2-11
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let gameStart = false;
-let firstCard = 2;
-let secondCard = 19;
-let Cards = [firstCard, secondCard]
-let newCard;
 
 let hasBlackjack = false;
-let isAlive = true;
 let message = ""
 let sum = firstCard + secondCard
 
@@ -18,7 +12,27 @@ let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 let newEl = document.getElementById("new-el");
 
+function getRandomCard() {
+
+    randomNumber = Math.floor(Math.random())*13 + 1
+    if (randomNumber > 10) {
+        randomNumber = 10;
+        
+    } else if (randomNumber === 1) {
+        randomNumber = 11;
+
+    } else {
+    }
+    return randomNumber
+}
+
 function startGame() {
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    let Cards = [firstCard, secondCard]
+    let isAlive = true;
+
+
     renderGame();
 }
 
@@ -38,12 +52,10 @@ function renderGame() {
     }
 
     messageEl.textContent = message;
-
+    cardsEl.textContent = "Cards: "
     for (var i = 0; i < Cards.length; i++) {
-    cardsEl.textContent += Cards[i];
+    cardsEl.textContent += Cards[i] + " ";
     }
-
-
     sumEl.textContent = "Sum: " + sum;
     gameStart = true;
 
@@ -51,11 +63,13 @@ function renderGame() {
 
 function newCard() {
   
-        newCard = 5;
+    if (isAlive == true && hasBlackjack == false) {
+        let newCard = getRandomCard();
         Cards.push(newCard)
         sum += newCard;
         renderGame();
-    
+
+    }    
 
 }
 
